2659b2a36ea40be78d083fe75d9cfaa4d9a984b0
Carlos Santos <Carlos Santos@LAPTOP-H2K9SUQI>
2022-03-14 12:11:57 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Digite un numero: ");
+            numero = int.Parse(Console.ReadLine());
+            while (numero != 0)
+            {
+                if (numero %2== 0)
+                {
+                    Console.WriteLine("El numero " + numero + " es PAR");
+                }
+                else
+                {
+                    Console.WriteLine("El numero " + numero + " es IMPAR");
+
+                }
+                Console.Write("Digite otro numero: ");
+                numero=int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
